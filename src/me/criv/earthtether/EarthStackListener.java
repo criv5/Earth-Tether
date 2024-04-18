1bd8becac3009abb637519eeba05eb1da8832e0a
criv <50537085+criv5@users.noreply.github.com>
2024-04-17 23:08:28 -0700

    Update EarthStackListener.java

--- src/me/criv/earthtether/EarthStackListener.java
+++ src/me/criv/earthtether/EarthStackListener.java
@@ -1,10 +1,10 @@
 package me.criv.earthtether;
 
 import com.projectkorra.projectkorra.BendingPlayer;
-import com.projectkorra.projectkorra.GeneralMethods;
 import com.projectkorra.projectkorra.ability.CoreAbility;
+import com.projectkorra.projectkorra.util.TempBlock;
 import org.bukkit.Location;
-import org.bukkit.block.Block;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,18 +12,47 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
-public class EarthStackListener implements Listener {
+public class EarthTetherListener implements Listener {
+    private int sourceRange = 25;
+    Location source = null;
+    private static boolean leftClickOccurred = false;
 
     @EventHandler
     public void onSneak(PlayerToggleSneakEvent event) {
+        if (!event.isSneaking()) return;
         Player player = event.getPlayer();
-        Location source = GeneralMethods.getTargetedLocation(player,10);
+        source = player.getTargetBlock(null, sourceRange).getLocation();
+        if (!source.getBlock().getType().isSolid())
+            source.add(0, -1, 0);
+        if (source.getBlock().getType() != Material.AIR) {
+            //TempBlock tempBlock = new TempBlock(source.getBlock(), Material.MUD);
+            //tempBlock.setRevertTime(10000);
+            BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+            if (bPlayer == null) return;
+            if (bPlayer.canBend(CoreAbility.getAbility(EarthTether.class))) {
+                EarthTether et = CoreAbility.getAbility(player, EarthTether.class);
+                if (et != null)
+                    et.remove();
+                new EarthTether(player, source);
+            }
+        }
+    }
 
-        player.sendMessage("we detected you sneaking");
-        BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+    @EventHandler
+    public void onLeftClick(PlayerInteractEvent event) {
+        if(event.getAction() != Action.LEFT_CLICK_AIR) return;
+        BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(event.getPlayer());
         if (bPlayer == null) return;
         if(bPlayer.canBend(CoreAbility.getAbility(EarthTether.class))) {
-            new EarthTether(player, source);
+            leftClickOccurred = true;
         }
+    }
+
+    public static boolean hasLeftClickOccurred() {
+        return leftClickOccurred;
+    }
+
+    public static void resetLeftClickOccured() {
+        leftClickOccurred = false;
     }
 }

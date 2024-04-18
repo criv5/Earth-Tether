2547b07b1de1e0cf44bd4650132618c57620cb67
criv <50537085+criv5@users.noreply.github.com>
2024-04-17 23:08:42 -0700

    Update EarthTether.java

--- src/me/criv/earthtether/EarthTether.java
+++ src/me/criv/earthtether/EarthTether.java
@@ -3,56 +3,70 @@ package me.criv.earthtether;
 import com.projectkorra.projectkorra.GeneralMethods;
 import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.ability.AddonAbility;
-import com.projectkorra.projectkorra.ability.EarthAbility;
+import com.projectkorra.projectkorra.ability.EarthAbility;;
+import com.projectkorra.projectkorra.util.TempBlock;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.Particle;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class EarthTether extends EarthAbility implements AddonAbility {
 
+    private Location source;
+
     private Listener listener;
     private Permission perm;
-    private Block fire;
-    private final int range = 25;
+    private final int sourceRange = 20;
+    private final int range = 35;
+    private final long interval = 1;
+    ArrayList<TempBlock> tempBlocks = new ArrayList<>();
 
     public EarthTether(Player player, Location source) {
         super(player);
-        //check if bendable block atsome point, then set it to different block type maybe
-        //if(isEarth(source.getBlock())) {
-            player.sendMessage("1 - block is earth...");
-            source.getBlock().setType(Material.MUD);
-            if(checkForEntity(player.getEyeLocation(), player.getLocation().getDirection()) != null) {
-                player.sendMessage("2 - entity found");
-                bPlayer.addCooldown(this);
-                start();
-            } else {
-                remove();
-            }
-        //} else {
-            //remove();
-        //}
+        this.source = source;
+        this.player = player;
+            start();
     }
 
-    private Entity checkForEntity(Location location, Vector direction) {
+    @Override
+    public void progress() {
+        if(EarthTetherListener.hasLeftClickOccurred()) {
+            EarthTetherListener.resetLeftClickOccured();
+
+            Entity target = checkForEntity(player.getEyeLocation(), player.getEyeLocation().getDirection());
+            if (source == null || target == null) {
+                remove();
+                return;
+            }
+
+            player.sendMessage("we saw that click, we gonna start right now");
+            bPlayer.addCooldown(this);
+
+            createLine(target.getLocation(), source, source.getBlock().getType(), interval, target);
+            this.source = null;
+            remove();
+        }
+        if (getStartTime() + 10000 < System.currentTimeMillis() && !EarthTetherListener.hasLeftClickOccurred()) {
+            player.sendMessage("time reset");
+            this.source = null;
+            remove();
+        }
+    }
+
+    public Entity checkForEntity(Location location, Vector direction) {
         List<Entity> entities = new ArrayList<>();
         for (int i = 0; i < range; i++) {
             location.add(direction);
-            location.getBlock().setType(Material.GLASS);
-
             entities.addAll(GeneralMethods.getEntitiesAroundPoint(location, 4)
                     .stream()
                     .filter(entity -> !entity.getUniqueId().equals(player.getUniqueId()))
@@ -62,35 +76,48 @@ public class EarthTether extends EarthAbility implements AddonAbility {
                 entities.get(0).setGlowing(true);
                 return entities.get(0);
             }
-            player.sendMessage("incrementing blocks... at " + i);
+            player.sendMessage("tracking beam block #: " + i);
         }
-        remove();
         return null;
     }
 
+    public void createLine(Location target, Location source, Material blockType, long interval, Entity entity) {
 
-    private void cwwdheckForPlayer(Location location, Vector direction) {
-        for (int i = 0; i < range; i++) {
-            location.add(direction);
-            location.getBlock().setType(Material.GLASS);
-            List<Entity> entities = GeneralMethods.getEntitiesAroundPoint(location, 4);
-            player.sendMessage("incrementing blocks... at " + i);
+        Vector direction = target.subtract(0,1,0).toVector().clone().subtract(source.toVector().clone()).normalize();
+        double targetDistance = source.distance(target);
+
+        for (int i = 0; i <= targetDistance; i++) {
+            Location location = source.clone().add(direction.clone().multiply(i));
+            int i1 = i;
+            new BukkitRunnable() {
+                public void run() {
+                    //if(location.getBlock().getType() != Material.AIR) return;
+                    player.sendMessage("createline at " + i1);
+                    TempBlock tempBlock = new TempBlock(location.getBlock(), source.getBlock().getType());
+                    tempBlock.setRevertTime(5000);
+                    tempBlocks.add(tempBlock);
+                    if(i1 >= targetDistance-1) {
+                        player.sendMessage("done now");
+                        retractLine(target, source, interval, entity);
+                    }
+                }
+            }.runTaskLater(ProjectKorra.plugin, interval * i);
         }
     }
 
-    private void setOnFire(Block block, BlockFace blockFace) {
-        fire = block.getRelative(blockFace);
-        fire.setType(Material.FIRE);
-    }
+    public void retractLine(Location target, Location source, long interval, Entity entity) {
+        double targetDistance = source.distance(target);
 
-    @Override
-    public void progress() {
-        Location loc = fire.getLocation();
-        loc.add(0.5,0.5,0.5);
-        loc.getWorld().spawnParticle(Particle.FLAME, loc, 5, 0.1,0.1,0.1);
-
-        if(getStartTime() + 3000 < System.currentTimeMillis()) {
-            remove();
+        for (int i = 0; i <= targetDistance; i++) {
+            new BukkitRunnable() {
+                public void run() {
+                   player.sendMessage("starting revert..." + tempBlocks.size());
+                    int last = tempBlocks.size() - 1;
+                    entity.teleport(tempBlocks.get(last).getLocation().add(0.5,1,0.5));
+                    tempBlocks.get(last).revertBlock();
+                    tempBlocks.remove(last);
+                }
+            }.runTaskLater(ProjectKorra.plugin, (10 + i * interval));
         }
     }
 
@@ -121,7 +148,7 @@ public class EarthTether extends EarthAbility implements AddonAbility {
 
     @Override
     public void load() {
-        listener = new EarthStackListener();
+        listener = new EarthTetherListener();
         ProjectKorra.plugin.getServer().getPluginManager().registerEvents(listener, ProjectKorra.plugin);
 
         perm = new Permission("bending.ability.earthtether");

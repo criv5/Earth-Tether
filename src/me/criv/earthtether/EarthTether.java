f115aa7bf5a675c41ed63a1382fa10dbd005a8a3
criv <50537085+criv5@users.noreply.github.com>
2024-04-21 21:05:23 -0700

    Update EarthTether.java

--- src/me/criv/earthtether/EarthTether.java
+++ src/me/criv/earthtether/EarthTether.java
@@ -3,8 +3,11 @@ package me.criv.earthtether;
 import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.ability.AddonAbility;
 import com.projectkorra.projectkorra.ability.EarthAbility;
+import com.projectkorra.projectkorra.configuration.ConfigManager;
 import com.projectkorra.projectkorra.util.TempBlock;
+import com.projectkorra.projectkorra.util.TempFallingBlock;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -21,104 +24,119 @@ import static me.criv.earthtether.EarthTetherListener.*;
 
 public class EarthTether extends EarthAbility implements AddonAbility {
 
-    private Location source;
+    public final int interval = 1;
+    private int range; //default 30
+    private int increment; //default 3
+    private int delay; //default 10
+    private long cooldown; //default 3000
+    private final Location source;
+    private final List<Entity> entities = new ArrayList<>();
+    private final ArrayList<TempBlock> tempBlocks = new ArrayList<>();
+    private final Set<Long> extendTimings = new HashSet<>();
+    private final Set<Long> retractTimings = new HashSet<>();
     private Entity target;
-
     private Listener listener;
     private Permission perm;
-    private final int range = 35;
-    private final int interval = 1;
-    private final int increment = 3;
-    private final int delay = 5;
     private long retractStart;
     private Vector direction;
     private Location location;
-    private List<Entity> entities = new ArrayList<>();
-    private ArrayList<TempBlock> tempBlocks = new ArrayList<>();
-    private Set<Long> extendTimings = new HashSet<>();
-    private ArrayList<Long> retractTimings = new ArrayList<>();
 
     public EarthTether(Player player, Location source) {
         super(player);
         this.source = source;
         this.player = player;
         if(!bPlayer.canBend(this)) return;
+        setConfig();
         start();
+        resetBeenDamaged();
+        resetLeftClickOccured();
     }
 
+    public void setConfig() {
+        this.range = ConfigManager.getConfig().getInt("ExtraAbilities.Criv.Earth.EarthTether.Range");
+        this.increment = ConfigManager.getConfig().getInt("ExtraAbilities.Criv.Earth.EarthTether.Increment");
+        this.delay = ConfigManager.getConfig().getInt("ExtraAbilities.Criv.Earth.EarthTether.Delay");
+        this.cooldown = ConfigManager.getConfig().getLong("ExtraAbilities.Criv.Earth.EarthTether.Cooldown");
+    }
     @Override
     public void progress() {
         if (hasLeftClickOccurred()) {
             resetLeftClickOccured();
+            if(player.getLocation().distance(source) > range) {
+                remove();
+            }
             long clickTime = getCurrentTick();
-            this.direction = player.getEyeLocation().getDirection().clone().normalize();
-            this.location = source.clone();
-            this.retractStart = clickTime + range * interval + delay;
+            direction = player.getEyeLocation().getDirection().clone().normalize();
+            location = source.clone();
+            retractStart = clickTime + range * interval + delay;
             for (int i = 0; i <= range; i++) {
                 extendTimings.add(clickTime + i * interval);
             }
-            player.sendMessage(String.valueOf(clickTime + range * interval));
             retractTimings.add(clickTime + range * interval);
             bPlayer.addCooldown(this);
         }
         if (extendTimings.contains(getCurrentTick())) {
             for (int i = 0; i < increment; i++) {
                 location.add(direction);
-                //player.sendMessage("createline at " + i);
                 TempBlock tempBlock = new TempBlock(location.getBlock(), source.getBlock().getType());
                 tempBlock.setRevertTime(15000);
-                this.tempBlocks.add(tempBlock);
+                tempBlocks.add(tempBlock);
                 player.getWorld().playSound(location, Sound.BLOCK_DEEPSLATE_BRICKS_PLACE, 2F, 0.1F);
 
-                this.entities.addAll(location.getWorld().getNearbyEntities(location, 2, 1, 2)
+                entities.addAll(location.getWorld().getNearbyEntities(location, 2, 1, 2)
                         .stream()
                         .sorted(Comparator.comparingDouble(entity -> entity.getLocation().distance(location)))
                         .filter(entity -> !entity.getUniqueId().equals(player.getUniqueId()) && entity instanceof LivingEntity)
                         .toList());
 
-                if (!this.entities.isEmpty()) {
-                    this.target = this.entities.get(0);
+                if (!entities.isEmpty()) {
+                    target = entities.get(0);
                     extendTimings.clear();
-                    this.retractStart = getCurrentTick() + delay;
+                    retractStart = getCurrentTick() + delay;
                 }
                 if(location.distance(source) > range) {
-                    this.target = null;
+                    target = null;
                     extendTimings.clear();
-                    this.retractStart = getCurrentTick() + delay;
+                    retractStart = getCurrentTick() + delay;
                 }
             }
         }
         if(retractTimings.contains(getCurrentTick())) {
             if (!tempBlocks.isEmpty()) {
                 for (int i = 0; i < increment; i++) {
-                    //player.sendMessage("starting revert..." + tempBlocks.size());
                     int last = tempBlocks.size() - 1;
-                    tempBlocks.get(last).revertBlock();
-                    player.getWorld().playSound(tempBlocks.get(last).getLocation(), Sound.BLOCK_DEEPSLATE_BRICKS_PLACE, 2F, 0.8F);
-                    //if (target == null) player.sendMessage("why null");
-                    if (target != null) {
-                        target.setVelocity(target.getLocation().toVector().clone().subtract(source.toVector().clone()).normalize().multiply(-this.increment));
+                    if (target != null && target.getLocation().distance(tempBlocks.get(last).getLocation()) < 5) {
+                        target.setVelocity(target.getLocation().toVector().clone().subtract(source.toVector().clone()).normalize().multiply(-increment));
                         if (source.distance(target.getLocation()) < 2) target.setVelocity(new Vector(0, 0, 0));
                         //entity.teleport(tempBlocks.get(last).getLocation().add(0.5, 0, 0.5));
                     }
+                    tempBlocks.get(last).revertBlock();
+                    player.getWorld().playSound(tempBlocks.get(last).getLocation(), Sound.BLOCK_DEEPSLATE_BRICKS_PLACE, 2F, 0.8F);
                     tempBlocks.remove(last);
                 }
             } else {
                 remove();
             }
         }
-        if (retractStart == getCurrentTick()) {
+        if(hasBeenDamaged()) {
+            for (TempBlock block : tempBlocks) {
+                block.revertBlock();
+                if(block.getBlock().getType() == Material.AIR) {
+                    TempFallingBlock tempFallingBlock = new TempFallingBlock(block.getLocation(), source.getBlock().getBlockData(), new Vector(0,0,0), this);
+                    tempFallingBlock.tryPlace();
+                }
+            }
+            remove();
+        }
+        if (retractStart <= getCurrentTick()) {
             if (!tempBlocks.isEmpty()) {
                 for (int i = 0; i < tempBlocks.size(); i++) {
                     retractTimings.add(getCurrentTick() + i * interval + delay);
                 }
             }
         }
-        if (getStartTime() + 10000 < System.currentTimeMillis() && !hasLeftClickOccurred()) {
-            player.sendMessage("time reset");
-            //this.source = null;
+        if (getStartTime() + 10000 < System.currentTimeMillis() && !hasLeftClickOccurred())
             remove();
-        }
     }
 
     @Override
@@ -128,12 +146,12 @@ public class EarthTether extends EarthAbility implements AddonAbility {
 
     @Override
     public boolean isHarmlessAbility() {
-        return false;
+        return true;
     }
 
     @Override
     public long getCooldown() {
-        return 2000;
+        return cooldown;
     }
 
     @Override
@@ -148,6 +166,11 @@ public class EarthTether extends EarthAbility implements AddonAbility {
 
     @Override
     public void load() {
+        ConfigManager.getConfig().addDefault("ExtraAbilities.Criv.Earth.EarthTether.Range", 30);
+        ConfigManager.getConfig().addDefault("ExtraAbilities.Criv.Earth.EarthTether.Increment", 3);
+        ConfigManager.getConfig().addDefault("ExtraAbilities.Criv.Earth.EarthTether.Delay", 4);
+        ConfigManager.getConfig().addDefault("ExtraAbilities.Criv.Earth.EarthTether.Cooldown", 2000);
+
         listener = new EarthTetherListener();
         ProjectKorra.plugin.getServer().getPluginManager().registerEvents(listener, ProjectKorra.plugin);
 
